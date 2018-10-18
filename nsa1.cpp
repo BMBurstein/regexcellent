dcaaa8721bb94625c5968ead6b5bd1b6e0e188e1
Baruch Burstein <bmburstein@gmail.com>
2018-10-18 23:51:50 +0300

    add equivelent chained build

--- nsa1.cpp
+++ nsa1.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[]) {
     std::cout << std::boolalpha;
 
     auto regex = cat( cat( cat( star( any<char>() ), lit('a') ), alt( lit('b'), lit('c') ) ), accept<char>() );
+    //auto regex = std::move(any<char>().star().concat(lit('a')).concat(std::move(lit('b').alt(lit('c')))).concat(accept<char>()));
 
     for(int i=1; i<argc; ++i) {
         std::cout << argv[i] << ":\t" << regex.run(argv[i], argv[i]+std::strlen(argv[i])) << std::endl;
