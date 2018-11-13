05e7039518443451473c6397b9754feb4f36fd1e
Baruch Burstein <bmburstein@gmail.com>
2018-11-13 17:32:06 +0200

    simpler testing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 #include <cstring>
 
 int main(int argc, char* argv[]) {
+    char* tests[] = {
+        nullptr,
+        "abe",
+        "abcde",
+        "acde",
+        "abaceacabdert"
+    };
+    if(argc == 1) {
+        argv = tests;
+        argc = sizeof(tests) / sizeof(tests[0]);
+    }
     std::cout << std::boolalpha;
 
     //auto regex = cat( cat( cat( cat( star( any<char>() ), lit('a') ), alt( lit('b'), lit('c') ) ), opt( lit('d') ) ), lit('e') ).compile();
